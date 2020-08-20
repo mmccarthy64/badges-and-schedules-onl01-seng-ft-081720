4e39ebaf3bc29239bcdca369a4bef9f6d303f461
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-20 20:52:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,6 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(attendees)
   attendees_badges = []
   attendees.each do |name|
-    return "Hello, my name is #{name}."
+    return ["Hello, my name is #{name}."]
   end
 end
